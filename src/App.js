815c72934c5ba4d214f64d727c631b2c6e277cf7
americanman <snowboardracer7@gmail.com>
2018-11-10 10:16:58 -0500

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: "",
+      count: 0
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users/')
+      .then(response => response.json())
+      .then(data => this.setState({
+        data
+      }))
+  }
+
+  reset = () => {
+    this.setState({
+      count: 0
+    })
+  }
+
+  oneIncrement = () => {
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      }
+    })
+  }
+
+  oneDecrement = () => {
+    this.setState(prevState => {
+      return {
+       count: prevState.count - 1 
+      }
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {Object.keys(this.state.data).map((key, i) =>
+        <div>{key}</div>
+        )}
+        <h1>Numbers</h1>
+        {this.state.count}
+        <hr/>
+        <button onClick={this.reset}>Reset</button><br/>
+        <button onClick={this.oneIncrement}>Increment by one</button> 
+        <button onClick={this.oneDecrement}>Increment by one</button> 
+
       </div>
     );
   }
